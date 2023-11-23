8b1d9d9a1196480db678f968be906a6cef09d097
itaivincent <itaivincent321@gmail.com>
2023-11-23 10:15:54 +0200

    creating assets

--- resources/views/assets/create.blade.php
+++ resources/views/assets/create.blade.php
@@ -1,5 +1,187 @@
 @extends('template.default')
 
 @section('content')
+<div id="content" class="main-content">
+            <div class="layout-px-spacing">
 
+                <div class="middle-content container-xxl p-0">
+
+                    <!-- BREADCRUMB -->
+                    <div class="page-meta">
+                        <nav class="breadcrumb-style-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Assets</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Create an asset</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <!-- /BREADCRUMB -->
+                        
+                    <div class="account-settings-container layout-top-spacing">
+    
+                        <div class="account-content">
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <h2>Create a User</h2>
+
+                                </div>
+                            </div>
+    
+                            <div class="tab-content" id="animateLineContent-4">
+                            
+                                <div class="tab-panel" id="animated-underline-profile" role="tabpanel" aria-labelledby="animated-underline-profile-tab">
+                                    <div class="row">
+                                    <form method="post" action="{{ route('users.store') }}">
+                                      @csrf   
+    
+                                        <div class="col-xl-12 col-lg-12 col-md-12 layout-spacing">
+                                            <div class="section general-info payment-info">
+                                                <div class="info">
+                                                    <h6 class="">Add Personal Details</h6>                              
+    
+                                                    <div class="row mt-4">
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Name</label>
+                                                                <input type="text" name="name" class="form-control add-billing-address-input">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Surname</label>
+                                                                <input type="text" name="surname" class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Address</label>
+                                                                <input type="text" name="address"  class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">City</label>
+                                                                <input type="text"  name="city" class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Country</label>                                              
+                                                                <input type="text"  name="country" class="form-control">
+                                                                                                                      
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Age</label>
+                                                                <input type="text"  name="age"  class="form-control">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+    
+                                                
+                                                </div>
+                                            </div>
+                                        </div>
+    
+                                        <div class="col-xl-12 col-lg-12 col-md-12 layout-spacing">
+                                            <div class="section general-info payment-info">
+                                                <div class="info">
+                                                    <h6 class="">Add Company Details</h6>                                     
+    
+                                                    <div class="row mt-4">
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Department</label>
+                                                                <div class="invoice-action-currency">
+                                                                    <div class="dropdown selectable-dropdown cardName-select">
+                                                                    <select name="department" class="form-select">
+                                                                    <option selected="">Choose...</option>
+                                                                    <option value="united-states">IT</option>
+                                                                    <option value="brazil">Operations</option>
+                                                                    <option value="indonesia">Accounts</option>
+                                                                    <option value="turkey">Management</option>
+                                                                    <option value="russia">Security</option>
+                                                                    <option value="india">HR</option>                                                          
+                                                                   </select>  
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+    
+                                                         <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Email Address</label>
+                                                                <input type="email" name="email"  class="form-control add-payment-method-input">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Phone Number</label>
+                                                                <input type="text" name="phoneNumber"  class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Employee Number</label>
+                                                                <input type="text" name="employeeNumber"  class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="mb-3">
+                                                                <label class="form-label">User Name</label>
+                                                                <input type="text"  name="userName"  class="form-control">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="col-md-6">
+                                                            <div  class="mb-3">
+                                                            <label class="form-label">User Role</label>
+                                                                <div class="invoice-action-currency">
+                                                                    <div class="dropdown selectable-dropdown cardName-select">
+                                                                    <select name="userRole" class="form-select">
+                                                                    <option selected="">Choose...</option>
+                                                                    @foreach ($roles as $role)  
+                                                                    <option value="{{ $role->id }}">{{$role->Name}}</option>
+                                                                    @endforeach                                                   
+                                                                   </select>  
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+    
+                                                  
+                                                </div>
+                                                
+                                            </div>
+                                            <button type="submit" class="btn btn-primary  float-end mt-3">Create User</button>
+                                        </div>
+                                    </form>
+                                    </div>
+                                </div>
+    
+                           
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!--  BEGIN FOOTER  -->
+            <div class="footer-wrapper">
+                <div class="footer-section f-section-1">
+                    <p class="">Copyright © <span class="dynamic-year">2022</span> <a target="_blank" href="https://designreset.com/cork-admin/">DesignReset</a>, All rights reserved.</p>
+                </div>
+                <div class="footer-section f-section-2">
+                    <p class="">Coded with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></p>
+                </div>
+            </div>
+            <!--  END FOOTER  -->
+            
+        </div>
 @endsection
