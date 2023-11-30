3b708ed017b356c65e467b4a875e976d5f0bb986
itaivincent <itaivincent321@gmail.com>
2023-11-30 15:40:30 +0200

    formulas

--- resources/views/contracts/parameters.blade.php
+++ resources/views/contracts/parameters.blade.php
@@ -71,7 +71,7 @@
                                 <div class="widget-content">
                                     <div class="w-header">
                                         <div class="w-info">
-                                            <h6 class="value">Rates</h6>
+                                            <h6 class="value">Formulas</h6>
                                         </div>
                                         <div class="task-action">
                                             <div class="dropdown">
@@ -80,8 +80,8 @@
                                                 </a>
 
                                                 <div class="dropdown-menu left" aria-labelledby="expenses" style="will-change: transform;">                                                
-                                                    <a class="dropdown-item"  data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">View All Rates</a>      
-                                                    <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#inputFormModal">Create Rate</a>                                            
+                                                    <a class="dropdown-item"  data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">View All Formula</a>      
+                                                    <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#inputFormulaModal">Create a Formula</a>                                            
                                                 </div>
                                             </div>
                                         </div>
@@ -256,6 +256,103 @@
                                             </div>
                                             <div class="modal-footer">                                            
                                                 <button type="submit" class="btn btn-primary mt-2 mb-2 btn-no-effect" data-bs-dismiss="modal">Create Route</button>
+                                            </div>
+                                            </form>
+                                          </div>
+                                        </div>
+                                    </div>
+
+
+
+
+
+                                    
+                                    <div class="modal fade inputForm-modal" id="inputFormulaModal" tabindex="-1" role="dialog" aria-labelledby="inputFormModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                          <div class="modal-content">
+  
+                                            <div class="modal-header" id="inputFormModalLabel">
+                                                <h5 class="modal-title">Create a <b>Formula</b></h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
+                                            </div>
+                                                                                       
+                                            <div class="modal-body">
+                                                <form  method="post" action="{{ route('contracts.formulaStore') }}"class="mt-0">
+                                                @csrf  
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <span class="input-group-text">
+                                                           
+                                                            </span>
+                                                            <input type="text"  name="from" class="form-control" placeholder="From" aria-label="name">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <span class="input-group-text">
+                                                     
+                                                            </span>
+                                                            <input type="text" name="to" class="form-control" placeholder="To" aria-label="password">
+                                                        </div>
+                                                    </div>   
+                                                    
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <span class="input-group-text">
+                                                   
+                                                            </span>
+                                                            <input type="text" name="activity" class="form-control" placeholder="Activity" aria-label="password">
+                                                        </div>
+                                                    </div> 
+
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <span class="input-group-text">
+                                                     
+                                                            </span>
+                                                            <input type="text" name="distance" class="form-control" placeholder="Distance" aria-label="password">
+                                                        </div>
+                                                    </div> 
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <span class="input-group-text">
+                                              
+                                                            </span>
+                                                            <input type="text" name="unit" class="form-control" placeholder="Unit" aria-label="password">
+                                                        </div>
+                                                    </div> 
+
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                           
+                                                                    <select name="routeCategory" class="form-select">
+                                                                    <option selected="">Choose Type</option>
+                                                                    <option value="Standard">Standard</option>
+                                                                    <option value="Adhoc">Adhoc</option>
+                                                                                                                                                                            
+                                                                   </select>  
+                                                                  
+                                                        </div>
+                                                    </div> 
+
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                           
+                                                                    <select name="type" class="form-select">
+                                                                    <option selected="">Choose Category</option>
+                                                                    <option value="Less than 1km">Less than 1km</option>
+                                                                    <option value="Short Haul">Short Haul</option>
+                                                                    <option value="Medium Haul">Medium Haul</option>  
+                                                                    <option value="Long Haul">Long Haul</option>                                                                                                                  
+                                                                   </select>  
+                                                                  
+                                                        </div>
+                                                    </div>
+  
+                                            </div>
+                                            <div class="modal-footer">                                            
+                                                <button type="submit" class="btn btn-primary mt-2 mb-2 btn-no-effect" data-bs-dismiss="modal">Create Formula</button>
                                             </div>
                                             </form>
                                           </div>
